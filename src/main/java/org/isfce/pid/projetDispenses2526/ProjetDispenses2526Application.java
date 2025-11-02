9a1ec4b6d3ef64c483e51dfc848c846169eaf3b4
mehdi lagh <mehdi.laghmich.dahmani@gmail.com>
2025-11-02 21:38:05 +0100

    Initial commit

---
+++ src/main/java/org/isfce/pid/projetDispenses2526/ProjetDispenses2526Application.java
@@ -0,0 +1,13 @@
+package org.isfce.pid.projetDispenses2526;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetDispenses2526Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetDispenses2526Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/isfce/pid/projetDispenses2526/ProjetDispenses2526ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.isfce.pid.projetDispenses2526;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetDispenses2526ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
